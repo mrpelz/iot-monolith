7ed499bd7ba80e00949ad11f0da37ab68eacac9b
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2018-08-05 12:04:06 +0200

    implement prometheus

---
+++ libs/prometheus/index.js
@@ -0,0 +1,25 @@
+// const { createServer } = require('http');
+// const { rebind } = require('../utils/oop');
+// const { Logger } = require('../log');
+
+// const logPrefix = 'obi-jack';
+// const { log } = new Logger(logPrefix);
+
+// class Prometheus {
+//   constructor(options) {
+//     const {
+//       port
+//     } = options;
+
+//     if (!port) {
+//       throw new Error('insufficient options provided');
+//     }
+
+//     rebind(this, '_handleRequest');
+//     this.on('connect', this._handleConnection);
+//   }
+// }
+
+// module.exports = {
+//   Prometheus
+// };
