b016d24e9274b7d98723e42e52f30691e8cb1460
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2018-10-06 10:20:10 +0200

    add handlers for unhandled exceptions

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const { createInstances, runLogic } = require('./app');
   createInstances();
   runLogic();
 }());
+
+process.on('uncaughtException', (error) => {
+  /* eslint-disable-next-line no-console */
+  console.error(`unhandled exception: ${error}`);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (error) => {
+  /* eslint-disable-next-line no-console */
+  console.error(`unhandled rejection: ${error}`);
+  process.exit(1);
+});
