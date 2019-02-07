9efa37423c6299f2797dc6f6be0e36dca4b72921
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2019-02-07 19:46:00 +0100

    add more testing for vent

--- libs/vent/tests/index.js
+++ libs/vent/tests/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const { Vent } = require('../index');
 const { resolveAlways } = require('../../../libs/utils/oop');
+const { sleep } = require('../../../libs/utils/time');
 
 // SINGLE-RELAY TEST
 const instance = new Vent({
@@ -15,7 +16,24 @@ instance.on('connect', async () => {
   console.log('actualIn', await resolveAlways(instance.getActualIn()));
   console.log('actualOut', await resolveAlways(instance.getActualOut()));
 
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(0)));
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(1)));
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(2)));
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(3)));
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(4)));
+  await sleep(5000);
   console.log('set target', await resolveAlways(instance.setTarget(5)));
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(6)));
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(7)));
+  await sleep(5000);
+  console.log('set target', await resolveAlways(instance.setTarget(0)));
 });
 
 instance.on('switch', (value) => {
