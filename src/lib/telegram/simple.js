752bab7c349cd522daf187b50c080ff2d3dd343b
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2019-09-08 16:42:43 +0200

    readd censored files (for real this time 🙄)

---
+++ src/lib/telegram/simple.js
@@ -0,0 +1,43 @@
+const { post } = require('../http/client');
+
+const host = 'api.telegram.org';
+const chatId = -274728913;
+
+const telegramHttpOptions = {
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8'
+  }
+};
+
+const telegramMessageOptions = {
+  parse_mode: 'Markdown',
+  disable_web_page_preview: true
+};
+
+function telegramSend(message) {
+  const {
+    telegramToken
+  } = global;
+
+  return post(
+    `https://${host}/bot${telegramToken}/sendMessage`,
+    Buffer.from(JSON.stringify(Object.assign(telegramMessageOptions, {
+      chat_id: chatId,
+      text: message
+    })), 'utf8'),
+    telegramHttpOptions
+  ).then((response) => {
+    const { ok, result } = JSON.parse(response);
+
+    if (!ok) {
+      throw new Error(JSON.stringify(result));
+    }
+  }).catch((error) => {
+    /* eslint-disable-next-line no-console */
+    console.log(`<3>error logging to telegram: "${error}"`);
+  });
+}
+
+module.exports = {
+  telegramSend
+};
