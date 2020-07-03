07ffca8daac78bbbaa8afce212c27e778bddfa08
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2020-07-03 11:52:31 +0200

    handle telegram token correctly

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@
 Error.stackTraceLimit = 50;
 process.stdin.resume();
 
-import { app } from './app/app.js';
 import { parseString } from './lib/utils/string.js';
-import { telegramSend } from './lib/telegram/simple.js';
 
 const env = {};
 
@@ -36,71 +34,87 @@ const env = {};
   });
 }());
 
-function telegramRoot(title = '', message, stack) {
-  if (!global.logTelegram) {
-    return Promise.resolve(null);
+(async function run() {
+  const [
+    {
+      app
+    },
+    {
+      telegramSend
+    }
+  ] = await Promise.all([
+    import('./app/app.js'),
+    import('./lib/telegram/simple.js')
+  ]).catch(() => {
+    process.exit(1);
+  });
+
+  function telegramRoot(title, message, stack) {
+    if (!global.logTelegram) {
+      return Promise.resolve(null);
+    }
+
+    return telegramSend(
+      [
+        '*ROOT*',
+        `_${title || ''}_`,
+        message || null,
+        stack ? `\`${stack}\`` : null
+      ].filter(Boolean).join('  \n')
+    ).then(() => {
+      return null;
+    }).catch(() => {
+      return null;
+    });
   }
 
-  return telegramSend(
-    [
-      '*ROOT*',
-      `_${title}_`,
-      message || null,
-      stack ? `\`${stack}\`` : null
-    ].filter(Boolean).join('  \n')
-  ).then(() => {
-    return null;
-  }).catch(() => {
-    return null;
-  });
-}
+  (function onStart() {
+    /* eslint-disable-next-line no-console */
+    console.log('<6>starting process');
+    telegramRoot('Starting process');
+  }());
 
-(function onStart() {
-  /* eslint-disable-next-line no-console */
-  console.log('<6>starting process');
-  telegramRoot('Starting process');
+  function quit(signal) {
+    process.nextTick(() => {
+      process.exit(signal);
+    });
+  }
+
+  /**
+   *
+   * @param {NodeJS.Signals} signal
+   */
+  function exit(signal = 'SIGINT') {
+    process.removeListener('SIGINT', exit);
+    process.removeListener('SIGTERM', exit);
+    process.removeListener('SIGUSR1', exit);
+    process.removeListener('SIGUSR2', exit);
+
+    /* eslint-disable-next-line no-console */
+    console.log('<6>stopping process');
+    telegramRoot('Stopping process', `Signal = ${signal}`).then(() => {
+      quit(signal);
+    });
+  }
+
+  process.on('uncaughtException', (error) => {
+    /* eslint-disable-next-line no-console */
+    console.log(`<0>uncaughtException: ${error.message}${error.stack ? `\n${error.stack}` : ''}`);
+    telegramRoot('uncaughtException', error.message, error.stack).then(() => {
+      exit();
+    });
+  });
+
+  process.on('unhandledRejection', (error) => {
+    /* eslint-disable-next-line no-console */
+    console.log(`<0>unhandledRejection: ${error.message}${error.stack ? `\n${error.stack}` : ''}`);
+    telegramRoot('unhandledRejection', error.message, error.stack);
+  });
+
+  process.on('SIGINT', exit);
+  process.on('SIGTERM', exit);
+  process.on('SIGUSR1', exit);
+  process.on('SIGUSR2', exit);
+
+  app(env);
 }());
-
-function quit(signal) {
-  process.nextTick(() => {
-    process.exit(signal);
-  });
-}
-
-/**
- *
- * @param {NodeJS.Signals} signal
- */
-function exit(signal = 'SIGINT') {
-  process.removeListener('SIGINT', exit);
-  process.removeListener('SIGTERM', exit);
-  process.removeListener('SIGUSR1', exit);
-  process.removeListener('SIGUSR2', exit);
-
-  /* eslint-disable-next-line no-console */
-  console.log('<6>stopping process');
-  telegramRoot('Stopping process', `Signal = ${signal}`).then(() => {
-    quit(signal);
-  });
-}
-
-process.on('uncaughtException', (error) => {
-  /* eslint-disable-next-line no-console */
-  console.log(`<0>uncaughtException: ${error.message}${error.stack ? `\n${error.stack}` : ''}`);
-  telegramRoot('uncaughtException', error.message, error.stack).then(() => {
-    exit();
-  });
-});
-
-process.on('unhandledRejection', (error) => {
-  /* eslint-disable-next-line no-console */
-  console.log(`<0>unhandledRejection: ${error.message}${error.stack ? `\n${error.stack}` : ''}`);
-  telegramRoot('unhandledRejection', error.message, error.stack);
-});
-
-process.on('SIGINT', exit);
-process.on('SIGTERM', exit);
-process.on('SIGUSR1', exit);
-process.on('SIGUSR2', exit);
-
-app(env);
