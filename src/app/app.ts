df34a380f629ef39a26d9e0a6b628894a6fd273a
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2021-09-04 18:15:58 +0200

    remove repl

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,44 +1,22 @@
 import { HttpServer } from '../lib/http-server.js';
 import { Tree } from '../lib/tree.js';
 import { WebApi } from '../lib/web-api.js';
-import { handleSignal } from '../index.js';
 import { logger } from './logging.js';
 import { office } from './rooms/office.js';
-import { start } from 'repl';
 
 const run = Date.now().toString();
 
-const log = logger.getInput({
-  head: 'app',
-});
+// const log = logger.getInput({
+//   head: 'app',
+// });
 
 const httpServer = new HttpServer(logger, 1337);
 httpServer.listen();
 
 export function app(): void {
-  const repl = start({
-    prompt: '🏠 ',
-  });
-
   const root = office(logger);
   const tree = new Tree(root);
 
   // eslint-disable-next-line no-new
   new WebApi(logger, httpServer, run, tree);
-
-  const { structure, values } = tree;
-
-  Object.assign(repl.context, {
-    root,
-    structure: () => {
-      log.info(() => JSON.stringify(structure, null, 2));
-    },
-    tree,
-    values: () => {
-      log.info(() => JSON.stringify(values(), null, 2));
-    },
-  });
-
-  process.on('beforeExit', () => repl.close());
-  repl.on('SIGINT', () => handleSignal('SIGINT'));
 }
