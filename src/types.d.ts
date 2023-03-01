cf18aa26d387c2c78b60435df47d7524c0167e1c
Lennart Pelz <mail@mrpelz.de>
2023-03-01 22:27:26 +0100

    tmp

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,5 @@
 declare namespace JSX {
-  type Element = import('./lib/tree/jsx').Base;
-  // type ElementAttributesProperty = Record<'props', never>
+  type Element = import('./lib/tree/jsx').Element;
   type ElementChildrenAttribute = Record<'children', never>
+  type IntrinsicElements = Record<'element', import('./lib/tree/jsx').Props>
 }
