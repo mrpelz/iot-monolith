5aacb78349ee5bdd63bb0afa08b19ee51093aff4
Lennart Pelz <mail@mrpelz.de>
2023-11-17 22:24:56 +0100

    chore: update  boilerplate-node, fix formatting

--- Makefile
+++ Makefile
@@ -1,1 +1,3 @@
-include node_modules/@mrpelz/boilerplate-common/config/Makefile
+BASE_FILE := $(shell npm ls --parseable --silent "@mrpelz/boilerplate-node" 2>/dev/null)
+
+include $(BASE_FILE)/Makefile
--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // @ts-ignore
-import config from '@mrpelz/boilerplate-common/config/eslint.config.js';
+import config from '@mrpelz/boilerplate-node/eslint.config.js';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default config;
