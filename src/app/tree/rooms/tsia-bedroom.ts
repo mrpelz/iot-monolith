ba2d69d766461ca49ea897144562006ee2acb77e
Lennart Pelz <mail@mrpelz.de>
2025-02-08 00:15:36 +0100

    feat: add tsiabedroom-nightlight

--- src/app/tree/rooms/tsia-bedroom.ts
+++ src/app/tree/rooms/tsia-bedroom.ts
@@ -1,3 +1,4 @@
+import { ev1527ButtonX1 } from '../../../lib/tree/devices/ev1527-button.js';
 import { ev1527WindowSensor } from '../../../lib/tree/devices/ev1527-window-sensor.js';
 import { obiPlug } from '../../../lib/tree/devices/obi-plug.js';
 import { shellyi3 } from '../../../lib/tree/devices/shelly-i3.js';
@@ -14,12 +15,18 @@ import { context } from '../../context.js';
 import { ev1527Transport } from '../bridges.js';
 
 export const devices = {
+  button: ev1527ButtonX1(4448, ev1527Transport, context),
   ceilingLight: sonoffBasic(
     'lighting' as const,
     'tsiabedroom-ceilinglight.lan.wurstsalat.cloud',
     context,
   ),
   doorSensor: ev1527WindowSensor(55_696, ev1527Transport, context),
+  nightLight: sonoffBasic(
+    'lighting' as const,
+    'mrpelzbedroom-nightlight.lan.wurstsalat.cloud',
+    context,
+  ),
   standingLamp: obiPlug(
     'lighting' as const,
     'tsiabedroom-standinglamp.lan.wurstsalat.cloud',
@@ -30,6 +37,8 @@ export const devices = {
 };
 
 export const instances = {
+  button: devices.button.state,
+  nightLightButton: devices.nightLight.button.state,
   standingLampButton: devices.standingLamp.button.state,
   wallswitchLeft: devices.wallswitch.button0.state,
   wallswitchMiddle: devices.wallswitch.button1.state,
@@ -39,16 +48,34 @@ export const instances = {
 export const properties = {
   ceilingLight: devices.ceilingLight.internal.relay,
   door: door(devices.doorSensor),
+  nightLight: devices.nightLight.internal.relay,
   standingLamp: devices.standingLamp.internal.relay,
   windowRight: window(devices.windowSensorRight),
 };
 
 export const groups = {
-  allLights: outputGrouping([properties.ceilingLight, properties.standingLamp]),
+  allLights: outputGrouping([
+    properties.ceilingLight,
+    properties.nightLight,
+    properties.standingLamp,
+  ]),
   allWindows: inputGrouping(properties.windowRight.open.main.state),
 };
 
 (() => {
+  instances.button.observe(() => {
+    if (groups.allLights.main.setState.value) {
+      groups.allLights.main.setState.value = false;
+      return;
+    }
+
+    properties.nightLight.flip.setState.trigger();
+  });
+
+  instances.nightLightButton.up(() =>
+    properties.nightLight.flip.setState.trigger(),
+  );
+
   instances.standingLampButton.up(() =>
     properties.standingLamp.flip.setState.trigger(),
   );
@@ -71,7 +98,7 @@ export const groups = {
   );
 
   instances.wallswitchRight.up(() =>
-    properties.ceilingLight.flip.setState.trigger(),
+    properties.nightLight.flip.setState.trigger(),
   );
   instances.wallswitchRight.longPress(
     () => (groups.allLights.main.setState.value = false),
